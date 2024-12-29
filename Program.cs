63515e975cf675c1d3a09931bfceec121325c78e
Alena Lisicyna <alelisicyna@gmail.com>
2024-12-29 06:34:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using DeepL;
+
+
+
+class Bot {
+  static void Main() {
+    // string authKey = "";
+    // Translator translator = new Translator(authKey);
+
+    using CancellationTokenSource cts = new CancellationTokenSource();
+    TelegramBotClient bot = new TelegramBotClient("", cancellationToken: cts.Token);
+    bot.OnMessage += OnMessage;
+
+    Console.WriteLine("--BOT RUN--");
+    Console.ReadLine();
+    cts.Cancel();
+
+    async Task OnMessage(Message msg, UpdateType type) {
+      if (msg.Text is null) {
+        return;
+      }
+
+      /* TranslateTextAsync translatedText = await translator.TranslateTextAsync(
+          msg.Text,
+          LanguageCode.Polish,
+          LanguageCode.English
+      ); 
+      await bot.SendMessage(msg.Chat, $"{translatedText}"); */
+      await bot.SendMessage(msg.Chat, $"{msg.Text}");
+      Console.WriteLine($"@{msg.From.Username} ({msg.From.Id}): {msg.Text}");
+    }
+  }
+}
